5766859484e052413a0180bf929337aefd7f3394
Pauline Kelly <pauline.m.kelly1@gmail.com>
2018-01-14 17:06:34 +1100

    Renamed player, add extra check for user input

--- ticTacToe.py
+++ ticTacToe.py
@@ -5,9 +5,9 @@ This is a small game between 2 players.
 """
 
 def store_names():
-    player_0 = input('Who will be naughts? ')
+    player_O = input('Who will be naughts? ')
     player_X = input('Who will be crosses? ')
-    return player_0, player_X
+    return player_O, player_X
 
 def display_game_board(grid):
     print('-' * 13)
@@ -18,8 +18,8 @@ def display_game_board(grid):
     print('| ' + grid[6] + ' | ' + grid[7] + ' | ' + grid[8] + ' |')
     print('-' * 13)
 
-def display_welcome(player_0, player_X):
-    print('Welcome ' + player_0 + ' and ' + player_X + '!')
+def display_welcome(player_O, player_X):
+    print('Welcome ' + player_O + ' and ' + player_X + '!')
 
 def create_game_grid():
     numbers = []
@@ -27,18 +27,33 @@ def create_game_grid():
         numbers.append(str(i))
     return numbers
 
-def check_valid_move(input_square, grid):
+def is_valid_number(input_square):
+    return input_square >= 0 and input_square <= 8
+
+def is_valid_move(input_square, grid):
     return grid[input_square] != 'X' and grid[input_square] != 'O'
 
-def place_symbol(grid, player, token):
-    input_square = int(input(player + ', select from 1-9 the spot you would like to choose.')) - 1
-    is_valid = check_valid_move(input_square, grid)
-
+def get_user_input(player, grid):
+    is_valid = False
     while not is_valid:
-        print("Sorry, that was not a valid move, please try again.")
-        input_square = int(input(player + ', select from 1-9 the spot you would like to choose.')) - 1
-        is_valid = check_valid_move(input_square, grid)
+        try:
+            input_square = int(input(player + ', select from 1-9 the spot you would like to choose.')) - 1
+            if not is_valid_number(input_square):
+                print("That's not a valid number on the game board. Try again.")
+                continue
+            if not is_valid_move(input_square, grid):
+                print("Someone's already placed a move here. Try again.")
+                continue
+            
+            # If we've got this far, the input is valid and can be used.
+            is_valid = True
 
+        except ValueError:
+            print("That's not a number! Try again.")
+    return input_square
+
+def place_symbol(grid, player, token):
+    input_square = get_user_input(player, grid)
     grid[input_square] = token
     return grid
 
@@ -62,16 +77,16 @@ def check_win(grid, token):
 
 def run():
     grid = create_game_grid()
-    player_0, player_X = store_names()
+    player_O, player_X = store_names()
 
-    token_map = {player_0:'O', player_X:'X'}
+    token_map = {player_O:'O', player_X:'X'}
 
-    display_welcome(player_0, player_X)
+    display_welcome(player_O, player_X)
     display_game_board(grid)
 
     for turn in range(9):
         if turn % 2 == 0:
-            current_player = player_0
+            current_player = player_O
         else:
             current_player = player_X
 
